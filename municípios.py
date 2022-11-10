2dbd6cea3b82e662cf9834d51171bd9c1030e9ba
Ramonn-prog <savioramonnogueira@gmail.com>
2022-11-10 19:51:10 -0300

    Create municípios.py

---
+++ municípios.py
@@ -0,0 +1,15 @@
+"""Faça um programa que leia os dados e preencha uma lista cujo conteúdo é a população dos 3 municípios
+mais populosos de cada um dos 26 estados brasileiros.
+    Determine e imprima:
+        a. imprima a média da população dos 3 municípios de cada estado.
+        b. considerando que a 2ª coluna (1) contém sempre a população da capital do estado, calcular a média da
+        população das capitais dos 26 estados;
+        c. o número do município (coluna) mais populoso e o nome do estado a que ele pertence, considerando que
+        não houve empate."""
+
+estados = []
+media = []
+populacao=[]
+municipio=[]
+
+
